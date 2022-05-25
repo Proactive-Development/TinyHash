c8736956c1da83ba7acb33a3e8f54f4a9db18f24
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2022-05-25 21:01:10 +0100

    Update small.py

--- src/modules/algorithms/small.py
+++ src/modules/algorithms/small.py
@@ -1,8 +1,3 @@
-"""
-1) HelloWorld
-2) 48656c6c6f576f726c64
-Final: 304f4
-"""
 def generate(text):
     raw = text
     output = str(len(raw)+len(text.encode("utf-8").hex()))+text.encode("utf-8").hex()[0]+text.encode("utf-8").hex()[round(len(text)/2)]+text.encode("utf-8").hex()[-1]
